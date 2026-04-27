aee09b186c27fdd5c33581a439a2cd3178f4de65
delanyo agbenyo <delanyodevio@gmail.com>
2026-04-27 20:33:14 +0000

    Feat: added topics

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,11 +1,5 @@
-import { tursoDB as db } from "$db/connections/turso.js";
-import { prepTable as table } from "$db/schema/preps";
 import { json } from "@sveltejs/kit";
 
 export const GET = async () => {
-  const creator_id = "113426c759eb76d09c4e1cd76";
-  const creator_name = "Delanyo Agbenyo";
-  await db.update(table).set({ creator_id, creator_name });
-
   return json("okay");
 };
